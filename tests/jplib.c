4040a9b36947e83291f5620e3f9fd0433b07a32b
Your Name <you@example.com>
2020-05-02 19:54:43 +0000

    seguim amb la mateixa merda del pagefault

--- tests/jplib.c
+++ tests/jplib.c
@@ -21,49 +21,45 @@ void init_scen(char scen[25][80]) {
 		scen[24][79] = '+';
 }
 
-
-
-
-
 void test_joc() {
 
 	char scen[25][80];
 	init_scen(scen);
 	int x = 12;
 	int y = 40;
-	char inp = 'U';
-	
+	char inp = 'U'; //0xb117ff 
 	scen[x][y] = '@';
 	while(inp != 'q') {
-	scen[0][0] = inp;
-		if (inp == 'h') {
+
+		get_key(&inp); //0xb11800
+		if (inp == 'h') { //0xb117ff
 			if(x > 1) {
-				scen[x][y] = ' ';
+				scen[x][y] = '\x00';
 				scen[--x][y] = '@';
 			}
 		}
 
 		if (inp == 'j') {
-			if(y < 78) {
-				scen[x][y] = ' ';
+			if(y < 78 ) {
+				scen[x][y] = '\x00';
 				scen[x][++y] = '@';
 			}
 		}
 
 		if (inp == 'k') {
 			if(y > 1) {
-				scen[x][y] = ' ';
+				scen[x][y] = '\x00';
 				scen[x][--y] = '@';
 			}
 		}
 		if (inp == 'l') {
 			if(x < 23) {
-				scen[x][y] = ' ';
+				scen[x][y] = '\x00';
 				scen[++x][y] = '@';
 			}
 		}
-		put_screen(scen);
-		get_key(&inp);
+	  put_screen(scen);
+		
 	}
 }
 
